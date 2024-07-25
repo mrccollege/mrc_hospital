e0a096c402c7f533db6a8132e1fdfe58f00af6e9
sanjay <sanjaypython22@gmail.com>
2024-07-25 19:28:26 +0530

    p

--- store/views.py
+++ store/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 
 from .models import MainStoreMedicine, Store, MainStoreMedicineTransaction, MainToMiniStoreMedicine, \
     MainToMiniStoreMedicineTransaction
+from medicine.models import Medicine
 
 
 # Create your views here.
 def main_store(request):
     store = MainStoreMedicine.objects.all()
-    store_name = store[0].store
+    if store:
+        store_name = store[0].store
+    else:
+        store_name = ''
     context = {
         'store_name': store_name,
         'store': store
@@ -35,9 +39,10 @@ def transfer_medicine_from_main(request):
         form = request.POST
         medicine_id = int(form.get('medicine_id'))
         mini_store_id = int(form.get('mini_store_id'))
+        medicine_name = form.get('medicine_name')
         transfer_medicine_qty = int(form.get('transfer_medicine_qty'))
         medicine = MainStoreMedicine.objects.get(medicine_id=medicine_id)
-        store_id = medicine.store.id
+        main_store_id = medicine.store.id
         existing_qty = medicine.qty
         if existing_qty >= transfer_medicine_qty:
             medicine_qty = existing_qty - transfer_medicine_qty
@@ -47,13 +52,13 @@ def transfer_medicine_from_main(request):
                 'status': status
             }
             return JsonResponse(context)
-        query = Q(medicine_id=medicine_id, from_store_id=store_id, to_store_id=mini_store_id)
+        query = Q(medicine_id=medicine_id, from_store_id=main_store_id, to_store_id=mini_store_id)
         is_medicine = MainToMiniStoreMedicine.objects.filter(query)
         if is_medicine:
             pre_qty = is_medicine[0].qty
             obj = MainToMiniStoreMedicine.objects.filter(query).update(qty=transfer_medicine_qty)
         else:
-            obj = MainToMiniStoreMedicine.objects.create(from_store_id=store_id,
+            obj = MainToMiniStoreMedicine.objects.create(from_store_id=main_store_id,
                                                          to_store_id=mini_store_id,
                                                          medicine_id=medicine_id,
                                                          qty=transfer_medicine_qty,
@@ -61,7 +66,15 @@ def transfer_medicine_from_main(request):
             obj_id = obj.id
         if obj:
             MainStoreMedicine.objects.filter(medicine_id=medicine_id).update(qty=medicine_qty)
-            MainToMiniStoreMedicineTransaction.objects.create()
+            medicine_obj = Medicine.objects.get(id=medicine_id)
+            MainToMiniStoreMedicineTransaction.objects.create(from_store_id=main_store_id,
+                                                              to_store_id=mini_store_id,
+                                                              medicine_id=medicine_id,
+                                                              medicine_name=medicine_name,
+                                                              qty=transfer_medicine_qty,
+                                                              medicine_manufacturer=medicine_obj.medicine_manufacturer,
+                                                              medicine_expiry=medicine_obj.medicine_expiry,
+                                                              )
             status = 'success'
         else:
             MainToMiniStoreMedicine.objects.filter(query).update(qty=pre_qty)
@@ -76,7 +89,10 @@ def transfer_medicine_from_main(request):
 
 def medicine_add_transaction_history(request):
     transaction = MainStoreMedicineTransaction.objects.all()
-    store_name = transaction[0].store
+    if transaction:
+        store_name = transaction[0].store
+    else:
+        store_name = ''
     context = {
         'store_name': store_name,
         'transaction': transaction,
@@ -94,7 +110,10 @@ def mini_store(request):
 
 def view_mini_store_medicine(request, id):
     store_medicine = MainToMiniStoreMedicine.objects.filter(to_store_id=id)
+    store_name = Store.objects.filter(id=id)
+    store_name = store_name[0].user
     context = {
-        'store_medicine': store_medicine
+        'store_medicine': store_medicine,
+        'store_name': store_name
     }
     return render(request, 'mini_store_medicine_detail.html', context)
