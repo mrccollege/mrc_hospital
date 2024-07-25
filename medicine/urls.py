b2695e625d781498cdca2461a8c39e1a8568d611
sanjay <sanjaypython22@gmail.com>
2024-07-25 14:21:22 +0530

    p

--- medicine/urls.py
+++ medicine/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('add_medicine/', views.add_medicine, name='add_medicine'),
+    path('view_medicine/<int:id>/', views.view_medicine, name='view_medicine'),
+    path('update_medicine/', views.update_medicine, name='update_medicine'),
     path('all_medicine/', views.all_medicine, name='all_medicine'),
 ]
--- medicine/views.py
+++ medicine/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 
@@ -44,7 +45,7 @@ def add_medicine(request):
 
                                                                     )
                         status = 'success'
-                        msg = 'Medicine successfully Added'
+                        msg = 'Medicine Added Successfully.'
 
         except Exception as e:
             status = status
@@ -56,6 +57,66 @@ def add_medicine(request):
         }
         return JsonResponse(context)
     return render(request, 'add_medicine.html')
+
+
+def view_medicine(request, id):
+    medicine_record = MainStoreMedicine.objects.get(id=id)
+    context = {
+        'medicine': medicine_record
+    }
+    return render(request, 'update_medicine.html', context)
+
+
+def update_medicine(request):
+    if request.method == 'POST':
+        form = request.POST
+        medicine_id = int(form.get('medicine_id'))
+        medicine_name = form.get('medicine_name')
+        medicine_qty = form.get('medicine_qty')
+        add_medicine_qty = form.get('add_medicine_qty')
+        minus_medicine_qty = form.get('minus_medicine_qty')
+        medicine_manufacturer = form.get('medicine_manufacturer')
+        medicine_expiry = form.get('medicine_expiry')
+        medicine_expiry = datetime.now().date()
+        status = 'failed'
+        msg = 'Medicine not added.'
+        try:
+            medicine_obj = Medicine.objects.filter(id=medicine_id).update(medicine_name=medicine_name,
+                                                                          medicine_manufacturer=medicine_manufacturer,
+                                                                          medicine_expiry=medicine_expiry,
+                                                                          )
+            if medicine_obj:
+                store_obj = Store.objects.filter(type='MAIN')
+                if store_obj:
+                    store_id = store_obj[0].id
+                    query = Q(medicine_id=medicine_id)
+                    obj = MainStoreMedicine.objects.get(query)
+                    total_qty = obj.qty
+                    if add_medicine_qty:
+                        total_qty = obj.qty + int(add_medicine_qty)
+
+                    if minus_medicine_qty:
+                        total_qty = obj.qty - int(minus_medicine_qty)
+                    main_store_medicine_obj = MainStoreMedicine.objects.filter(query).update(qty=int(total_qty))
+                    if main_store_medicine_obj:
+                        MainStoreMedicineTransaction.objects.create(store_id=store_id,
+                                                                    medicine_name=medicine_name,
+                                                                    qty=medicine_qty,
+                                                                    medicine_manufacturer=medicine_manufacturer,
+                                                                    medicine_expiry=medicine_expiry,
+                                                                    )
+                        status = 'success'
+                        msg = 'Medicine Updated Successfully.'
+
+        except Exception as e:
+            status = status
+            msg = str(e)
+
+        context = {
+            'status': status,
+            'msg': msg,
+        }
+        return JsonResponse(context)
 
 
 def all_medicine(request):
