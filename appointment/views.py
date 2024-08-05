1462d9bf656c30256633873e20c97f4bde58f950
sanjay <sanjaypython22@gmail.com>
2024-08-05 13:18:43 +0530

    hello

--- appointment/views.py
+++ appointment/views.py
@@ -5,17 +5,30 @@ from django.shortcuts import render
 
 from patient.models import Patient
 
+from doctor.models import Doctor
+
 
 # Create your views here.
 def add_appointment(request):
-    return render(request, 'add_appointment.html')
+    doctor = Doctor.objects.all()
+    context = {
+        'doctor': doctor
+    }
+    return render(request, 'add_appointment.html', context)
 
 
 def search_patient(request):
     if 'term' in request.GET:
-        # print(request.GET.get('term'), '===============@@@@@@@@@@@@@@@@@@@@@@@@@@@')
         qs = Patient.objects.filter(patient_code__icontains=request.GET.get('term'))
-        suggestions = list(qs.values_list('user__username', flat=True))
-        print(suggestions, '======================suggestions================')
-        return JsonResponse(suggestions, safe=False)
+        data_list = []
+        for i in qs:
+            data_dict = {}
+            data_dict['id'] = i.id
+            data_dict['name'] = i.user.username
+            data_dict['code'] = i.patient_code
+            data_list.append(data_dict)
+        context = {
+            'data_list': data_list
+        }
+        return JsonResponse(context, safe=False)
     return JsonResponse([], safe=False)
