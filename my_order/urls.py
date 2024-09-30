005f557d6050154f3929d1ccff7ae037881f15fe
sanjay <sanjaypython22@gmail.com>
2024-09-30 15:56:55 +0530

    p

--- my_order/urls.py
+++ my_order/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('medicine_order/', views.medicine_order, name='medicine_order'),
+    path('update_medicine_order/<int:id>/', views.update_medicine_order, name='update_medicine_order'),
+    path('delete_medicine_order/<int:id>/', views.delete_medicine_order, name='delete_medicine_order'),
     path('my_medicine_ordered_list/', views.my_medicine_ordered_list, name='my_medicine_ordered_list'),
     path('search_medicine/', views.search_medicine, name='search_medicine'),
 ]
--- my_order/views.py
+++ my_order/views.py
@@ -129,3 +129,104 @@ def my_medicine_ordered_list(request):
         'order': order,
     }
     return render(request, 'my_odered_list.html', context)
+
+
+def update_medicine_order(request, id):
+    if request.method == 'POST':
+        form = request.POST
+        medicines = json.loads(request.POST.get('medicines'))
+        doctor_id = form.get('doctor_id')
+        subtotal = form.get('sub_total')
+        discount = form.get('total_discount')
+        shipping = form.get('shipping_packing')
+        pay_amount = form.get('total')
+        try:
+            order_head = MedicineOrderHead.objects.create(doctor_id=doctor_id,
+                                                          subtotal=subtotal,
+                                                          discount=discount,
+                                                          shipping=shipping,
+                                                          pay_amount=pay_amount,
+                                                          )
+
+            if order_head:
+                for medicine_data in medicines:
+                    medicine_id = medicine_data['medicine_id']
+                    order_qty = medicine_data['order_qty']
+                    mrp = medicine_data['mrp']
+                    amount = medicine_data['amount']
+
+                    MedicineOrderDetail.objects.create(head_id=order_head.id,
+                                                       medicine_id=medicine_id,
+                                                       mrp=mrp,
+                                                       order_qty=order_qty,
+                                                       amount=amount,
+                                                       )
+
+                status = 'success'
+                msg = 'order successfully created.'
+
+            else:
+                status = 'failed'
+                msg = 'order failed.'
+
+        except Exception as e:
+            status = 'failed'
+            msg = str(e)
+
+        context = {
+            'status': status,
+            'msg': msg,
+        }
+
+        return JsonResponse(context)
+
+    else:
+        user_id = request.session.get('user_id')
+        try:
+            user = User.objects.get(id=user_id)
+            doctor = Doctor.objects.get(user_id=user.id)
+            doctor_id = doctor.id
+            user_id = doctor.user.id
+        except:
+            doctor = ''
+            doctor_id = 0
+            user_id = 0
+
+        order_head = MedicineOrderHead.objects.filter(id=id)
+        order_detail = MedicineOrderDetail.objects.filter(head_id=id)
+        context = {
+            'order_head': order_head,
+            'order_detail': order_detail,
+            'user_id': user_id,
+            'doctor': doctor,
+            'doctor_id': doctor_id,
+        }
+        print(context, '=============context=')
+        return render(request, 'update_medicine_order.html', context)
+
+
+def delete_medicine_order(request, id):
+    user_id = request.session.get('user_id')
+    try:
+        user = User.objects.get(id=user_id)
+        doctor = Doctor.objects.get(user_id=user.id)
+        doctor_id = doctor.id
+    except:
+        doctor_id = 0
+
+    if doctor_id != 0:
+        MedicineOrderHead.objects.filter(id=id).delete()
+        MedicineOrderDetail.objects.filter(head_id=id).delete()
+
+        status = 'success'
+        msg = 'data deleted'
+    else:
+        status = 'failed'
+        msg = 'data not deleted'
+
+    context = {
+        'status': status,
+        'msg': msg,
+    }
+    print(context, '==========context')
+    return JsonResponse(context)
