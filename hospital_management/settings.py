5317fe18fc0c20f8d4a9f07dc7859eb978f8e59e
sanjay <sanjaypython22@gmail.com>
2024-11-17 09:29:09 +0530

    p

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,28 +89,28 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ayurveda_live',
-        'USER': 'root',
-        'PASSWORD': 'Hello12345678#$@',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ayurveda',
-#         'USER': 'sanjay',
+#         'NAME': 'ayurveda_live',
+#         'USER': 'root',
 #         'PASSWORD': 'Hello12345678#$@',
-#         'HOST': '145.223.18.243',
+#         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.mysql'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
