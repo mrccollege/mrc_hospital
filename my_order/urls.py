be7fb6f7d6fdb42ef3270bece25eec0861960fd2
sanjay <sanjaypython22@gmail.com>
2024-11-23 12:03:18 +0530

    p

--- my_order/urls.py
+++ my_order/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('view_estimate/<int:id>/', views.view_estimate, name='view_estimate'),
 
     path('view_normal_invoice/<int:id>/', views.view_normal_invoice, name='view_normal_invoice'),
+    path('view_normal_invoice_doctor/<int:id>/', views.view_normal_invoice_doctor, name='view_normal_invoice_doctor'),
     path('view_estimate_invoice/<int:id>/', views.view_estimate_invoice, name='view_estimate_invoice'),
 ]
