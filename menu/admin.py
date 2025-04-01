1186f053e4310037c91bab75895e6ae4be276021
sanjay <sanjaypython22@gmail.com>
2025-04-01 15:29:54 +0530

    p

--- menu/admin.py
+++ menu/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import MenuCategory, MenuMaster, MenuUser
+from .models import MenuCategory, MenuMaster, MenuUser, MenuPurpose
 
 
 # Register your models here.
+class MenuPurposeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'created_at', 'id')
+
+
 class MenuCategoryAdmin(admin.ModelAdmin):
     list_display = ('cat_title', 'cat_desc', 'created_at', 'id')
 
@@ -15,6 +19,7 @@ class MenuUserAdmin(admin.ModelAdmin):
     list_display = ('menu', 'user', 'created_at', 'id')
 
 
+admin.site.register(MenuPurpose, MenuPurposeAdmin)
 admin.site.register(MenuCategory, MenuCategoryAdmin)
 admin.site.register(MenuMaster, MenuMasterAdmin)
 admin.site.register(MenuUser, MenuUserAdmin)
