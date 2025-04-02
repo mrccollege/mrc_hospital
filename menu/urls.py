c0ff0d6764d6615107fc9b015b12145ce390ee60
sanjay <sanjaypython22@gmail.com>
2025-04-02 15:19:16 +0530

    p

--- menu/urls.py
+++ menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('assign_menu/', views.assign_menu, name='assign_menu'),
     path('get_user_data/', views.get_user_data, name='get_user_data'),
     path('get_menu_data/', views.get_menu_data, name='get_menu_data'),
+    path('save_selected_menus/', views.save_selected_menus, name='save_selected_menus'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+import json
 
+from django.views.decorators.csrf import csrf_exempt
 from .models import MenuMaster, MenuUser, MenuCategory
 from store.models import Store
 
@@ -58,17 +60,52 @@ def get_menu_data(request):
     if request.method == 'GET':
         form = request.GET
         menu_type = int(form.get('menu_type'))
+        user_id = int(form.get('user_id'))
+        assign_menus = MenuUser.objects.filter(menu__menu_category_id=menu_type, user_id=user_id).values_list('menu', flat=True)
         menus = MenuMaster.objects.filter(menu_category_id=menu_type)
-        print(menus, '================menus')
         menus_data_list = []
         for i in menus:
             data_dict = {}
-            data_dict['user_id'] = i.user.id
-            data_dict['username'] = i.user.username
-            data_dict['user_mobile'] = i.user.mobile
+            data_dict['menu_id'] = i.id
+            data_dict['menu_title'] = i.menu_title
+            data_dict['menu_url'] = i.menu_url
+            if i.id in assign_menus:
+                data_dict['is_selected'] = True
+            else:
+                data_dict['is_selected'] = False
             menus_data_list.append(data_dict)
 
         context = {
             'menus_data_list': menus_data_list,
         }
         return JsonResponse(context)
+
+
+@csrf_exempt  # Only use if CSRF token is not handled in AJAX
+def save_selected_menus(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            selectedMenuIds = data.get("selectedMenuIds", [])
+            unselectedMenuIds = data.get("unselectedMenuIds", [])
+            user_id = data.get("user_id")
+
+            for menu_id in selectedMenuIds:
+                already = MenuUser.objects.filter(menu_id=menu_id, user_id=user_id).exists()
+                if already:
+                    pass
+                else:
+                    MenuUser.objects.create(menu_id=menu_id,user_id=user_id,)
+
+            for menu_id in unselectedMenuIds:
+                already = MenuUser.objects.filter(menu_id=menu_id, user_id=user_id).exists()
+                if already:
+                    MenuUser.objects.filter(menu_id=menu_id, user_id=user_id).delete()
+                else:
+                    pass
+
+            return JsonResponse({"success": True, "message": "Menus saved successfully!"})
+        except Exception as e:
+            return JsonResponse({"success": False, "message": str(e)})
+
+    return JsonResponse({"success": False, "message": "Invalid request method."})
